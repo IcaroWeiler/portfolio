49f5fdb2bd97a7bf3cb7b1606f35920a1b304241
Icaro Weiler <icaro.weiler@easyb2b.io>
2024-11-28 18:59:25 -0300

    test assetprefix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  assetPrefix: "./",
+  assetPrefix: "https://icaroweiler.github.io/portfolio",
   basePath: "/portfolio",
   images: {
     unoptimized: true,
