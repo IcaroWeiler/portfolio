3a9eba105312f73cca3f4a40c0e69d331568efee
Icaro Weiler <icaro.weiler@easyb2b.io>
2024-11-30 17:36:11 -0300

    revert cors

--- app/api/send/route.tsx
+++ app/api/send/route.tsx
@@ -15,35 +15,11 @@ export async function POST(req: Request) {
     });
 
     if (error) {
-      return new Response(JSON.stringify({ error }), {
-        status: 500,
-        headers: corsHeaders(),
-      });
+      return Response.json({ error }, { status: 500 });
     }
 
-    return new Response(JSON.stringify(data), {
-      status: 200,
-      headers: corsHeaders(),
-    });
+    return Response.json(data);
   } catch (error) {
-    return new Response(JSON.stringify({ error: String(error) }), {
-      status: 500,
-      headers: corsHeaders(),
-    });
+    return Response.json({ error: String(error) }, { status: 500 });
   }
-}
-
-// Middleware para lidar com CORS
-export function OPTIONS() {
-  return new Response(null, {
-    headers: corsHeaders(),
-  });
-}
-
-function corsHeaders() {
-  return {
-    "Access-Control-Allow-Origin": "*", // Substitua '*' pelo domínio permitido, se necessário
-    "Access-Control-Allow-Methods": "POST, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type",
-  };
 }
