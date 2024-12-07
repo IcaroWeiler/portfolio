a7fcb8ca9cb5937b913904e0ae09bd458c246c7a
Icaro Weiler <icaro.weiler@easyb2b.io>
2024-12-06 21:06:04 -0300

    updates next config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/portfolio",
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
+  output: "standalone",
 };
 
 export default nextConfig;
